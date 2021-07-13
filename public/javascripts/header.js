55d0997a5381d11b09c66fd32ae60d8bea1ca00e
Jonatan Cespedes <retratos.jonatan@gmail.com>
2021-07-13 19:22:35 -0300

    agrega funcionalidades al header, version responsive

--- public/javascripts/header.js
+++ public/javascripts/header.js
@@ -1,22 +1,39 @@
-// Capturo los elementos del DOM
-let navBar = document.getElementById("navigation-bar");
+
+/* Capturo los elementos del DOM */
+let navBar = document.getElementById("navigation-bar-mobile");
 let searchBar = document.getElementById("search");
 
-function dropMenu(){
+function dropMenu(){    
+    let subcategoryMenu = document.querySelector(".active");
     if (navBar.style.display === "block") {
         navBar.style.display = "none";
+        subcategoryMenu.classList.remove("active")
     } else {
         navBar.style.display = "block";
         searchBar.style.display = "none";
+        subcategoryMenu ? subcategoryMenu.classList.remove("active") : ""
     }
 }
 
-
 function dropSearch(){
+    let subcategoryMenu = document.querySelector(".active");
     if (searchBar.style.display === "block") {
         searchBar.style.display = "none";
+        subcategoryMenu.classList.remove("active")
     } else {
         searchBar.style.display = "block";
         navBar.style.display = "none";
+        subcategoryMenu.classList.remove("active")
     }
 }
+
+function dropSubCategoryMenu (id) {
+    let list = document.getElementById(`${id}`)
+    list.classList.toggle("active")
+    navBar.style.display = "none";
+    searchBar.style.display = "none";
+}
+
+ function closeWindow () {
+     document.querySelector(".active").classList.remove("active")
+ }
